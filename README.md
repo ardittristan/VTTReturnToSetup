21f67875ca7ff771476362e14584176a50c8a1dd
ardittristan <ardittristan@gmail.com>
2020-08-14 01:23:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# VTT ReturnToSetup
+
+Adds a way to return to setup from the login screen.
+
+## Installation
+
+To install, import this [Manifest](https://raw.githubusercontent.com/ardittristan/VTTReturnToSetup/master/module.json) into your module browser.
+
+## Usage
+
+Log in at least once as a gm, then a new user should be generated. If you log in as that user you get sent to the setup (should probably keep the user without a password)
+
+## Changelog
+
+Check the [Changelog](https://github.com/ardittristan/VTTReturnToSetup/blob/master/CHANGELOG.md)
---
+++ main.js
@@ -0,0 +1,42 @@
+
+Hooks.once('getUserContextOptions', () => {
+    const name = game.user.name;
+    if (ciIncludes(name, "return") && ciIncludes(name, "to") && ciIncludes(name, "setup")) {
+        if (sessionStorage.getItem("returnedToSetup") === "true") {
+            sessionStorage.removeItem("returnedToSetup")
+            game.logOut();
+        } else {
+            game.shutDown();
+            sessionStorage.setItem("returnedToSetup", "true")
+        }
+        
+        
+    }
+    if (game.user.isGM) {
+        let createUser = true;
+        game.users.forEach(/** @param {User} user */(user) => {
+            console.log(user.name)
+            if (ciIncludes(user.name, "return") && ciIncludes(user.name, "to") && ciIncludes(user.name, "setup")) {
+                createUser = false
+            }
+        });
+        if (createUser) {
+            createUserObject();
+        }
+    }
+});
+
+/**
+ * @param  {String} input
+ * @param  {String} query
+ */
+function ciIncludes(input, query) {
+    return input.toLowerCase().includes(query.toLowerCase());
+}
+
+async function createUserObject() {
+    const user = await User.create({
+        name: "Return to Setup",
+        role: CONST.USER_ROLES.GAMEMASTER
+    });
+}
